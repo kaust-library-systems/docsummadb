8626a5c3fc9136fa3744fa51b54ec0751bbc2ccc
Marcelo Garcia <marcelo.garcia@kaust.edu.sa>
2025-08-26 15:35:09 +0300

    Reverted changes to db config

--- docsummadb.py
+++ docsummadb.py
@@ -17,11 +17,11 @@ def main():
     lg.info(f"CSV file path: {csv_file}")
 
     # Open the Chroma db
-    db_dir = Path.cwd() / "db" / "mydb"
+    db_dir = Path.cwd() / "db"
     db = chromadb.PersistentClient(path=db_dir)
     from chromadb.api.types import EmbeddingFunction
 
-    collection = db.get_or_create_collection(name="my_collection")
+    collection = db.get_or_create_collection(name="archives")
 
     with open(csv_file, newline='', encoding='utf-8') as ff:
         csv_reader = reader(ff, dialect='unix')
